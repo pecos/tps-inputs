422c730f63a247537e6adfc96589e487f4124044
milindasf <milinda@oden.utexas.edu>
2024-03-14 08:33:45 -0500

    minor fix

--- axisymmetric/argon/lowP/six-species-maxwell-rates/gen_par.py
+++ axisymmetric/argon/lowP/six-species-maxwell-rates/gen_par.py
@@ -1,15 +1,16 @@
 import configparser
-output_folder = "par_ss/ls6"
+output_folder = "par_ss"
 base_file     = "plasma.6sp.tps2boltzmann.ini"
 lxcat_path    = "/scratch/03727/tg830270/tps_apptainer/tps/boltzmann/BESolver/python/lxcat_data/eAr_crs.6sp_Tg_0.5eV" 
 sub_clusters  = [2048, 1024, 512, 256, 128]
+node_count    = [1, 2, 4, 8, 16]
 
 config = configparser.ConfigParser()
 config.read(base_file)
 config["boltzmannSolver"]["collisions"]=lxcat_path
 for i in range(len(sub_clusters)):
     config["boltzmannSolver"]["n_sub_clusters"] = str(sub_clusters[i])
-    with open (output_folder+"/plasma.6sp.tps2boltzmann_%d.ini"%(i),"w") as f:
+    with open (output_folder+"/plasma.6sp.tps2boltzmann_ss_%d.ini"%(node_count[i]),"w") as f:
         config.write(f)
         f.close()
         
