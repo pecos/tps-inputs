86573f12c0287662a9181f843a85f87b854e56f6
milindasf <milinda@cs.utah.edu>
2024-03-19 23:10:46 -0500

    python script to generate par files

--- axisymmetric/argon/lowP/six-species-maxwell-rates/gen_par.py
+++ axisymmetric/argon/lowP/six-species-maxwell-rates/gen_par.py
@@ -1,20 +1,33 @@
 #!/bin/python3
 import configparser
+import argparse
 
-output_folder = "./"
+parser = argparse.ArgumentParser()
+parser.add_argument("-output_folder" , "--output_folder"           , help="output folder for tps-bte param files", type=str, default="./")
+parser.add_argument("-ngpus_per_node", "--ngpus_per_node"          , help="GPUs per node", type=int, default=4)
+parser.add_argument("-solver_type"   , "--solver_type"             , help="solver type", type=str, default="steady-state")
+parser.add_argument("-out_fname"     , "--out_fname"               , help="output file name for the qois", type=str, default="bte_ss")
+parser.add_argument("-lxcat"         , "--lxcat"                   , help="absolute path for lxcat file" , type=str, default="")
+parser.add_argument("-sub_clusters"  , "--sub_clusters"            , help="sub cluster size", nargs='+', type=int, default=[1024, 512, 256, 128, 64])
+parser.add_argument("-node_count"    , "--node_count"              , help="sub cluster size", nargs='+', type=int, default=[1, 2, 4, 8, 16])
+
+args = parser.parse_args()
+
+output_folder = args.output_folder
 base_file     = "plasma.6sp.tps2boltzmann.ini"
-lxcat_path    = "/scratch/03727/tg830270/tps-bte/tps/boltzmann/BESolver/python/lxcat_data/eAr_crs.6sp_Tg_0.5eV" 
-sub_clusters  = [1024, 512, 256, 128, 64]
-node_count    = [1, 2, 4, 8, 16]
+lxcat_path    = args.lxcat
+sub_clusters  = args.sub_clusters
+node_count    = args.node_count
 
 config = configparser.ConfigParser()
 config.optionxform=str
 config.read(base_file)
 
 config["boltzmannSolver"]["collisions"]   = lxcat_path
-config["boltzmannSolver"]["solver_type"]  = "steady-state"
-config["boltzmannSolver"]["output_fname"] = "bte_ss"
-config["gpu"]["numGpusPerRank"]           = str(3)
+config["boltzmannSolver"]["solver_type"]  = args.solver_type
+config["boltzmannSolver"]["output_fname"] = args.out_fname
+config["boltzmannSolver"]["threads"]      = str(4)
+config["gpu"]["numGpusPerRank"]           = str(args.ngpus_per_node)
 
 for i in range(len(sub_clusters)):
     config["boltzmannSolver"]["n_sub_clusters"] = str(sub_clusters[i])
